4124c6e2be92eab1525d410729cb752a9d9db3c6
Jeet Kanodia <jeetkanodia@gmail.com>
2023-03-23 22:04:32 +0530

    the UI for the game

--- script.js
+++ script.js
@@ -1,25 +1,54 @@
+var playerScore=0;
+var ComputerScore = 0;
+
 function getComputerChoice()
 {
+    var result='';
     let a = Math.floor(Math.random() * 3);
     if (a==0)
-    return "rock";
+    result= "rock";
     else if (a==1)
-    return "paper";
+    result= "paper";
     else 
-    return "scissors";
+    result= "scissors";
+
+    return result;
+
+}
+//playerwin and computer win needs to be added
+function playerWin(){
+    window.alert("THE PLAYER WINS!!!");
+    playerScore=0;
+    ComputerScore = 0;
 }
 
-function playRound(playerSelection , computerSelection)
+function computerWin(){
+    window.alert("You lost, the computer won");
+    playerScore=0;
+    ComputerScore = 0;
+}
+
+function checkForWinner(){
+    if(playerScore==5){
+        playerWin();
+    }
+    if(ComputerScore==5){
+        computerWin();
+    }
+}
+function playRound(playerSelection)
 {
+    computerSelection = getComputerChoice();
+    var result ='';
     switch(playerSelection)
     {
         case "rock": 
         {
             switch(computerSelection)
             {
-                case "rock": return "It's a Tie!"; break;
-                case "paper": return "You lose! Paper beats Rock";break;
-                case "scissors": return "You win! Rock beats Scissors";break;
+                case "rock": result= "It's a Tie!"; break;
+                case "paper": result= "You lose! Paper beats Rock";ComputerScore+=1;break;
+                case "scissors": result= "You win! Rock beats Scissors";playerScore+=1;break;
             }
         }
         break;
@@ -28,9 +57,9 @@ function playRound(playerSelection , computerSelection)
         {
             switch(computerSelection)
             {
-                case "paper": return "It's a Tie!"; break;
-                case "scissors": return "You lose! Scissors beats Paper";break;
-                case "rock": return "You win! Paper beats Rock";break;       
+                case "paper": result= "It's a Tie!"; break;
+                case "scissors": result= "You lose! Scissors beats Paper";ComputerScore+=1;break;
+                case "rock": result= "You win! Paper beats Rock";playerScore+=1;break;       
             }
         }
         break;
@@ -38,18 +67,14 @@ function playRound(playerSelection , computerSelection)
         {
             switch(computerSelection)
             {
-            case "scissors": return "It's a Tie!"; break;
-            case "rock": return "You lose! Rock beats Scissors";break;
-            case "paper": return "You win! Scissors beats Paper";break;       
+            case "scissors": result= "It's a Tie!"; break;
+            case "rock": result= "You lose! Rock beats Scissors"; ComputerScore+=1;break;
+            case "paper": result= "You win! Scissors beats Paper";playerScore+=1; break;       
             }
         }
     }
-}
+    var div = document.getElementById("result");
+    div.innerHTML = result;
 
-function game()
-{
-    player = "paper" ;
-    computer = getComputerChoice().toLowerCase();
-    console.log(playRound(player, computer))
+    checkForWinner();
 }
-game();
